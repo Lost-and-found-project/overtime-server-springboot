8d6423e71a82a3aad6541a8bdb74ff63e818e69f
ForteScarlet <ForteScarlet@163.com>
2021-10-19 16:06:39 +0800

    Update config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,11 @@
 rootProject.name = "overtime-server-springboot"
 
-
-
+// include("overtime-application")
+// include("overtime-gateway")
 includes("overtime-configuration") {
     sub("configuration-handler") // 异常处理、响应值处理
     sub("configuration-dataSource") // 数据源配置
 }
-
-include("overtime-gateway")
 
 includes("overtime-common") {
     sub("common-core")
@@ -16,8 +14,8 @@ includes("overtime-common") {
     sub("common-controller")
 }
 
-// include("overtime-application")
-// include("overtime-gateway")
+include("overtime-application")
+include("overtime-gateway")
 
 val modules = listOf(
     "domain", "repository",
@@ -53,33 +51,35 @@ annotation class IncDsl
 annotation class IncOuterDsl
 
 class Include(parentPath: String?, path: String) {
-    val path: String = if (parentPath != null) "$parentPath:$path" else path
-    // val name: String = if (parentPath != null) "$parentPath-$path" else path
+    private val path: String =
+        if (parentPath != null) "$parentPath:$path" else path
+    private val name: String =
+        if (parentPath != null) "$parentPath-$path" else path
 
     @Suppress("NOTHING_TO_INLINE")
-    inline fun doInc(ignoreIfNotExists: Boolean = false) {
+    fun doInc(ignoreIfNotExists: Boolean = false) {
         if (ignoreIfNotExists) {
             kotlin.runCatching {
-                include(path).run {
-                    println(" >>>> '$path' BuildFileName: " + project(path).buildFileName)
-                    println(" >>>> '$path' name: " + project(path).name)
-                }
+                include(path)
             }.getOrElse {
                 println("> [Warn]: Include $path failed: ${it.localizedMessage}")
             }
         } else {
-            include(path).run {
-                println(" >>>> '$path' BuildFileName: " + project(path).buildFileName)
-                println(" >>>> '$path' name: " + project(path).name)
-            }
+            include(path)
         }
         println("> Include $path")
+        val p = project(":$path")
+        println(">> Project: $p")
+        println(">> Project.name: ${p.name}")
+        println(">> Name: $name")
+        println()
+        // p.name = name
     }
 
 
     @IncDsl
     @Suppress("NOTHING_TO_INLINE")
-    inline fun sub(path: String, ignoreIfNotExists: Boolean = false, block: (Include.() -> Unit) = {}) {
+    fun sub(path: String, ignoreIfNotExists: Boolean = false, block: (Include.() -> Unit) = {}) {
         val inc = Include(this.path, path)
         inc.doInc(ignoreIfNotExists)
         block(inc)
@@ -88,7 +88,7 @@ class Include(parentPath: String?, path: String) {
 
 @IncOuterDsl
 @Suppress("NOTHING_TO_INLINE")
-inline fun includes(id: String, block: (Include.() -> Unit) = {}) {
+fun includes(id: String, block: (Include.() -> Unit) = {}) {
     val inc = Include(null, id)
     inc.doInc()
     block(inc)
@@ -97,7 +97,7 @@ inline fun includes(id: String, block: (Include.() -> Unit) = {}) {
 
 @IncOuterDsl
 @Suppress("NOTHING_TO_INLINE")
-inline fun Include.subs(ignoreIfNotExists: Boolean = false) {
+fun Include.subs(ignoreIfNotExists: Boolean = false) {
     // sub("domain", ignoreIfNotExists)
     // sub("repository", ignoreIfNotExists)
     modules.forEach {
